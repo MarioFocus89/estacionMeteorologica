b055726267d29cdec34ea8a896af175b59f5ba4b
MarioFocus89 <marioayusopadilla@gmail.com>
2018-05-12 18:19:07 +0200

    Version definitiva Python

--- Estacion_meteo.py
+++ Estacion_meteo.py
@@ -5,12 +5,15 @@ import time
 import datetime
 
 now = datetime.datetime.now()
+print "Almacenando datos en /home/pi/Desktop/Datos/"+now.strftime("%Y-%m-%d_%H:%M:%S")+".csv"
 dataFile = open("/home/pi/Desktop/Datos/"+now.strftime("%Y-%m-%d_%H:%M:%S")+".csv", "w")
 dataFile.write("Fecha,Metano,Temperatura(C),Humedad(%),Presion(mbar)\n")
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 txt = ''
 
 try:
+    print "Configurando dispositivo..."
+    time.sleep(5)
     while (True):
         now = datetime.datetime.now()
         txt = now.strftime("%Y-%m-%d %H:%M:%S")+',';
